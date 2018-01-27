e04ff9ecbf2c607b2d47f0cb095664ad47ce8a73
Mroczeks <piotrmatusik2001@gmail.com>
2018-01-27 23:12:08 +0100

    Update login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_controller.consider_all_requests_local = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-   config.force_ssl = true 
+   config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
